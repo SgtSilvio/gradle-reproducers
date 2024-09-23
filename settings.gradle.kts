e95d6e1bd013400fe153186b4175b6669890b58e
Silvio Giebl <gieblsilvio@outlook.com>
2024-09-23 22:17:26 +0200

    Isolate wrongly-filtered-artifact-results project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.name = "gradle-reproducers"
 
-include("wrongly-filtered-artifact-results")
+includeBuild("wrongly-filtered-artifact-results")
 includeBuild("wrongly-queried-property-with-configuration-cache")
---
+++ wrongly-filtered-artifact-results/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "wrongly-filtered-artifact-results"
